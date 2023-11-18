282f501276aaf79093796f6cd3b14afd093c373e
Thiago Linhares <thn.linhares@gmail.com>
2023-11-18 15:12:14 -0300

    Player implementado

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
@@ -7,60 +5,109 @@ public class Player : MonoBehaviour
     public float moveSpeed = 5f;
     public float jumpForce = 5f;
     private Rigidbody2D rb;
-    private bool isGrounded = true;
-    public PlayerAnimator playerAnimator;
+    private bool isGrounded;
+    private Animator animator;
+    private SpriteRenderer spriterend;
 
+    // Inicialização
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        spriterend = GetComponent<SpriteRenderer>();
     }
 
+    // Atualização a cada frame
     void Update()
     {
-        float x = Input.GetAxis("Horizontal");
-
-        // Verifica se o personagem está se movendo
-        if (x != 0)
-        {
-            Move(x);
-            playerAnimator.PlayAnimation("Run"); // Ativa animação de corrida
-        }
-        else if (isGrounded)
-        {
-            ;// playerAnimator.PlayAnimation("Player_idle"); // Ativa animação de parado
-        }
-
-        // Verifica se o personagem deve pular
+        MovePlayer();
         if (Input.GetKeyDown(KeyCode.UpArrow) && isGrounded)
         {
             Jump();
         }
+        UpdateAnim();
+    }
 
-        // Verifica se o personagem está caindo
-        if (!isGrounded && rb.velocity.y < 0)
+    // Método para movimentar o jogador
+    void MovePlayer()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(moveX * moveSpeed, rb.velocity.y);
+        Flip(moveX);
+
+        if(moveX > 0f)
         {
-            playerAnimator.PlayAnimation("Fall"); // Ativa animação de queda
+            spriterend.flipX = false;
+
+        }
+        if (moveX < 0f)
+        {
+            animator.SetBool("Walk", true);
+            spriterend.flipX = true;
+        }
+        if (moveX == 0f)
+        {
+            animator.SetBool("Idle", true);
+        }
+
+    }
+    // Método para flip
+
+    void Flip(float flip)
+    {
+        if (flip < 0f)
+        {
+            spriterend.flipX = true;
+            animator.SetBool("Walk", true);
+
+        } else if (flip > 0f)
+        {
+            animator.SetBool("Walk", true);
+            spriterend.flipX = false;
+        }
+        if (flip == 0f)
+        {
+            animator.SetBool("Idle", true);
         }
     }
 
-    void Move(float x)
-    {
-        Vector2 movement = new Vector2(x * moveSpeed, rb.velocity.y);
-        rb.velocity = movement;
-    }
-
+    // Método para pular
     void Jump()
     {
         rb.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
-        isGrounded = false;
-        playerAnimator.PlayAnimation("Jump"); // Ativa animação de pulo
+        animator.SetBool("Jump", true);
     }
 
+    // Verificar se o jogador está no chão
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Ground"))
         {
             isGrounded = true;
+            animator.SetBool("Idle", true);
+            animator.SetBool("Jump", false);
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;
+            
+        }
+    }
+
+    void UpdateAnim()
+    {
+        if (!isGrounded && rb.velocity.y < 0)
+        {
+            animator.SetBool("Fall", true);
+            animator.SetBool("Jump", false);
+        }
+        else
+        {
+            animator.SetBool("Fall", false);
         }
     }
 }
