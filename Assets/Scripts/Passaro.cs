d9f5ac392acf71fe056867ade7fa3c4c896340e2
Thiago Linhares <thn.linhares@gmail.com>
2023-11-19 00:10:06 -0300

    Implementando cena1

--- Assets/Scripts/Passaro.cs
+++ Assets/Scripts/Passaro.cs
@@ -3,19 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Saw : MonoBehaviour
+public class Passaro : MonoBehaviour
 {
+    private const bool V = false;
+
     // Start is called before the first frame update
 
-    public float speed;
+    public float speed = 4f;
     private bool dirRight = true;
     private float timer;
     private GameObject gameobj;
+    private SpriteRenderer spriterend;
+    private Rigidbody2D rb;
 
-    public float movetime;
+    public float movetime = 3f;
     void Start()
     {
+        spriterend = GetComponent<SpriteRenderer>();
+        rb = GetComponent<Rigidbody2D>();
 
+        rb.mass = 0f;
+        rb.gravityScale = 0f;
+     
     }
 
     // Update is called once per frame
@@ -24,10 +33,12 @@ public class Saw : MonoBehaviour
         if (dirRight)
         {
             transform.Translate(Vector2.right * speed * Time.deltaTime);
+            spriterend.flipX = V;
         }
         else
         {
             transform.Translate(Vector2.left * speed * Time.deltaTime);
+            spriterend.flipX = true;
         }
 
         timer += Time.deltaTime;
